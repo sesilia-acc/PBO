87b38e8eb26d6a8e1be2417c69ee515e1a67971a
sesilia-acc <dandinsesilia@gmail.com>
2026-09-06 15:09:06 -0800

    modif

--- jobsheet2/id/ac/polinema/Main.java
+++ jobsheet2/id/ac/polinema/Main.java
@@ -2,10 +2,18 @@ package jobsheet2.id.ac.polinema;
 
 public class Main {
     public static void main(String[] args) {
-        Account original = new Account("Nadia", 500000);
-        Account copy = original;
-        copy.deposit(100000);
-        System.out.println("Via original: " + original.balance);
-        System.out.println("Via copy: " + copy.balance);
+        Account[] accounts = new Account[3];
+
+        accounts[0] = new Account("Nadia", 500000);
+        accounts[0].withdraw(150000);
+
+        accounts[1] = new Account("Budi", 100000);
+
+        accounts[2] = new Account("Sari", 750000);
+        accounts[2].withdraw(250000);
+
+        for (Account account : accounts) {
+            account.printInfo();
+        }
     }
 }
